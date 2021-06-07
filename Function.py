5c9486abf272a2a43dad5a0f23a699d831e26444
Your Name <lisajp4@gmail.com>
2021-06-07 10:12:46 +0000

    Added Functions to the repo

---
+++ Function.py
@@ -0,0 +1,20 @@
+def add_calc(number1, number2):
+    answer = number1 + number2
+    return answer
+
+added_number = add_calc(5,5)
+
+print(added_number + 20)
+
+def add_calc(a, b, c):
+	answer = a + b + c
+	return answer
+
+a = 23
+b = 47
+c = 67
+
+added_number = add_calc(a, b, c)
+
+print((added_number/175) * 100)
+
