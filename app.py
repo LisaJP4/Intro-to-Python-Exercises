f18f89189a77c445afc3e407d99f979beccdebcd
Lisa Pigott <thedo@python3.europe-west2-c.c.virtual-machine-trial-315013.internal>
2021-06-08 15:45:19 +0000

    Updated dice

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-import dice
+import boss
 
+boss.bad_boss("Bill Steves")
+
+import boss
+boss.bad_team("Manchester United")
+
+import Throw
+print(("You have thrown two dice. Your numbers are..."), Throw.dice())
--- dice.py
+++
@@ -1,6 +0,0 @@
-def dice(number_1, number_2, number_3, number_4,number_5, number_6):
-	print(throw)
-
-throw = 
-dice(1, 2, 3, 4, 5, 6)
-
