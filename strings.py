359f31cf30c0b1494d03c16a85329a6840e991a0
Lisa Pigott <thedo@python.europe-west2-c.c.virtual-machine-trial-315013.internal>
2021-06-10 21:57:42 +0000

    Updating files today

--- strings.py
+++ strings.py
@@ -14,3 +14,28 @@ def string_match(a, b):
   return count
 
 print(string_match("Mississippi", "LucazadeSport"))
+
+def date_fashion(you, date):
+  yes = 2
+  maybe = 1
+  no = 0
+  if you >= 8 or date >= 2:
+    return yes
+  elif you <= 2 or date <= 2:
+    return no
+  else:
+    return maybe
+
+print(date_fashion(7, 2))
+
+
+def squirrel_play(temp, is_summer):
+  if temp in range(60, 100) and is_summer == True:
+    return True
+  elif is_summer == False and temp in range(60, 90):
+    return True
+  else:
+    return False
+
+
+print(squirrel_play(90, False))
